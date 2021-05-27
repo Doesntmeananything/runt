3eae8317efa2addb37e4ca0e1c8d81560c6332a5
Doesn'tmeananything <andrey.krasnov.pm@gmail.com>
2021-05-27 23:33:58 +0300

    Add basic tui setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, io};
+
+use termion::raw::IntoRawMode;
+use tui::{
+    backend::TermionBackend,
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders},
+    Terminal,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let stdout = io::stdout()
+        .into_raw_mode()
+        .expect("Failed to switch to raw mode");
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend).expect("Failed to initialize terminal backend");
+    terminal
+        .draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(80),
+                        Constraint::Percentage(10),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+            let block = Block::default().title("Block").borders(Borders::ALL);
+            f.render_widget(block, chunks[0]);
+            let block = Block::default().title("Block 2").borders(Borders::ALL);
+            f.render_widget(block, chunks[1]);
+        })
+        .expect("Failed to draw to terminal");
+
+    Ok(())
 }
